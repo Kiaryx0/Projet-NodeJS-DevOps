5332fca3a9259d7c3e1bd497a6ec17cc16421a1c
Louis <louisdeveze78@gmail.com>
2019-12-10 17:54:46 +0100

    adding Database population script
    Adding the script that populate the database when running npm run populate

---
+++ bin/populate.ts
@@ -0,0 +1,52 @@
+#!/usr/bin/env ts-node
+
+import { Metric, MetricsHandler } from '../src/metrics'
+import { User, UserHandler } from '../src/users'
+
+const users = [
+  new User('Louis', 'louis.deveze@edu.ece.fr' , 'Framboise'),
+  new User('Maxime','maxime.tran@edu.ece.fr' ,'Litchi'),
+]
+
+const met1 = [
+  new Metric(`${new Date('2013-11-04 14:00 UTC').getTime()}`, 12),
+  new Metric(`${new Date('2013-11-04 14:15 UTC').getTime()}`, 10),
+  new Metric(`${new Date('2013-11-04 14:30 UTC').getTime()}`, 8),
+  new Metric(`${new Date('2013-11-04 14:45 UTC').getTime()}`, 23),
+  new Metric(`${new Date('2013-11-04 15:30 UTC').getTime()}`, 25),
+  new Metric(`${new Date('2013-11-04 15:15 UTC').getTime()}`, 19),
+  new Metric(`${new Date('2013-11-04 15:45 UTC').getTime()}`, 17)
+]
+
+const met2 = [
+  new Metric(`${new Date('2013-11-04 14:00 UTC').getTime()}`, 67),
+  new Metric(`${new Date('2013-11-04 14:15 UTC').getTime()}`, 64),
+  new Metric(`${new Date('2013-11-04 14:30 UTC').getTime()}`, 58),
+  new Metric(`${new Date('2013-11-04 14:45 UTC').getTime()}`, 48),
+  new Metric(`${new Date('2013-11-04 15:15 UTC').getTime()}`, 73),
+  new Metric(`${new Date('2013-11-04 15:40 UTC').getTime()}`, 65),
+  new Metric(`${new Date('2013-11-04 17:15 UTC').getTime()}`, 59),
+  new Metric(`${new Date('2013-11-04 16:30 UTC').getTime()}`, 47)
+]
+
+const dbUser = new UserHandler('./db/users')
+const dbMetrics = new MetricsHandler('./db/metrics')
+
+dbUser.saveMany(users, (err: Error | null) => {
+    if (err) throw err
+    console.log('Users were added to the Database')
+})
+
+dbMetrics.save("maxime" ,met1, (err: Error | null) => {
+  if (err) throw err
+  console.log('Metrics of Maxime were added to the Database')
+})
+
+
+dbMetrics.save("louis" ,met2, (err: Error | null) => {
+  if (err) throw err
+  console.log('Metrics of Louis were added to the Database')
+})
+
+dbUser.close();
+dbMetrics.close();
